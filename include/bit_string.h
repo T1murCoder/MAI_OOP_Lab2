97bbe178813b7b3dc2d4056444579d3c1e7298c8
Timur <110349339+T1murCoder@users.noreply.github.com>
2025-10-10 19:33:37 +0000

    Fixed bit_string

--- include/bit_string.h
+++ include/bit_string.h
@@ -5,15 +5,17 @@
 
 class BitString {
 public:
-    BitString();
-    BitString(const size_t& arraySize, unsigned char defaultValue = 0);
+    BitString(); // конструктор по умолчанию (правило пяти)
+    BitString(const size_t& arraySize, unsigned char defaultValue = 0); // конструктор с параметром (правило пяти)
     BitString(const std::initializer_list<unsigned char>& initialValues);
     BitString(const std::string& sourceString);
 
     BitString(const BitString& other); // копирующий конструктор (правило пяти)
     BitString(BitString&& other); // перемещающий конструктор (правило пяти)
     virtual ~BitString() noexcept; // деструктор (правило пяти)
-    // тут еще не хватает до правила пяти копирующего и перемащающего оператора присваивания
+
+    BitString add(const BitString& other) const;
+    BitString subtract(const BitString& other) const;
 
     BitString bitAnd(const BitString& other) const;
     BitString bitOr(const BitString& other) const;
@@ -21,9 +23,11 @@ public:
     BitString bitNot() const;
 
     bool equals(const BitString& other) const;
-    // bool less(const BitString& other) const;
-    // bool greater(const BitString& other) const;
+    bool less(const BitString& other) const;
+    bool greater(const BitString& other) const;
     std::string toString() const;
+
+    size_t getSize() const;
 private:
     size_t arraySize;
     unsigned char* bitArray;
--- src/bit_string.cpp
+++ src/bit_string.cpp
@@ -18,6 +18,10 @@ BitString::BitString(const std::initializer_list<unsigned char>& initialValues) 
 
     size_t index = 0;
     for (const auto& value: initialValues) {
+        if (value != (char) 0 && value != (char) 1) {
+            delete[] bitArray;
+            throw std::invalid_argument("Некорректные значения в переданном массиве");
+        }
         bitArray[index++] = value;
     }
 }
@@ -31,18 +35,18 @@ BitString::BitString(const std::string& sourceString) {
             delete[] bitArray;
             throw std::invalid_argument("Строка должна состоять только из '0' или '1'");
         }
-        bitArray[i] = static_cast<unsigned char>(sourceString[i]);
+        bitArray[i] = (sourceString[i] == '1') ? 1 : 0;
     }
 }
 
-BitString::BitString(const BitString& other) : arraySize(other.arraySize) {
+BitString::BitString(const BitString& other) : arraySize(other.getSize()) {
     bitArray = new unsigned char[arraySize];
     for (size_t i = 0; i < arraySize; ++i) {
         bitArray[i] = other.bitArray[i];
     }
 }
 
-BitString::BitString(BitString&& other) : bitArray(other.bitArray), arraySize(other.arraySize) {
+BitString::BitString(BitString&& other) : bitArray(other.bitArray), arraySize(other.getSize()) {
     other.bitArray = nullptr;
     other.arraySize = 0;
 }
@@ -56,62 +60,107 @@ BitString::~BitString() noexcept {
     arraySize = 0;
 }
 
+// Тут просто складываем массивы и возвращаем новый объект
+BitString BitString::add(const BitString& other) const {
+
+    
+    return BitString(toString() + other.toString());
+}
+
+// Тут просто вычитаем размеры массивов и возвращаем новый объект с урезанным массивом
+BitString BitString::subtract(const BitString& other) const {
+    
+    if (getSize() < other.getSize()) {
+        throw std::logic_error("Размер массива не может быть отрицательным");
+    }
+
+    size_t newSize = getSize() - other.getSize();
+    
+    if (newSize == 0) {
+        return BitString();
+    }
+    
+    return BitString(toString().substr(0, newSize));
+}
+
 // Тут ввел ограничение с одинаковой длиной строк для упрощения 
 BitString BitString::bitAnd(const BitString& other) const {
-    if(arraySize != other.arraySize) {
+    if(getSize() != other.getSize()) {
         throw std::invalid_argument("Строки должны иметь одинаковую длину для побитовой операции");
     }
 
-    BitString result(arraySize);
-    for (size_t i = 0; i < arraySize; ++i) {
+    BitString result(getSize());
+    for (size_t i = 0; i < getSize(); ++i) {
         result.bitArray[i] = (bitArray[i] & other.bitArray[i]);
     }
     return result;
 }
 
 BitString BitString::bitOr(const BitString& other) const {
-    if(arraySize != other.arraySize) {
+    if(getSize() != other.getSize()) {
         throw std::invalid_argument("Строки должны иметь одинаковую длину для побитовой операции");
     }
 
-    BitString result(arraySize);
-    for (size_t i = 0; i < arraySize; ++i) {
+    BitString result(getSize());
+    for (size_t i = 0; i < getSize(); ++i) {
         result.bitArray[i] = (bitArray[i] | other.bitArray[i]);
     }
     return result;
 }
 
 BitString BitString::bitXor(const BitString& other) const {
-    if(arraySize != other.arraySize) {
+    if(getSize() != other.getSize()) {
         throw std::invalid_argument("Строки должны иметь одинаковую длину для побитовой операции");
     }
 
-    BitString result(arraySize);
-    for (size_t i = 0; i < arraySize; ++i) {
+    BitString result(getSize());
+    for (size_t i = 0; i < getSize(); ++i) {
         result.bitArray[i] = (bitArray[i] ^ other.bitArray[i]);
     }
     return result;
 }
 
 BitString BitString::bitNot() const {
-    BitString result(arraySize);
-    for (size_t i = 0; i < arraySize; ++i) {
+    BitString result(getSize());
+    for (size_t i = 0; i < getSize(); ++i) {
         result.bitArray[i] = (bitArray[i] == 0) ? 1 : 0;
     }
     return result;
 }
 
 bool BitString::equals(const BitString& other) const {
-    if (arraySize != other.arraySize) {
+    if (getSize() != other.getSize()) {
         return false;
     }
 
-    for (size_t i = 0; i < arraySize; ++i) {
+    for (size_t i = 0; i < getSize(); ++i) {
         if (bitArray[i] != other.bitArray[i]) {
             return false;
         }
     }
     return true;
+}
+
+bool BitString::greater(const BitString& other) const {
+    return !equals(other) && !less(other); 
+}
+
+bool BitString::less(const BitString& other) const {
+    if (getSize() < other.getSize()) {
+        return true;
+    }
+    if (getSize() > other.getSize()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < getSize(); ++i) {
+        if (bitArray[i] < other.bitArray[i]) {
+            return true;
+        } else if (bitArray[i] > other.bitArray[i]) {
+            return false;
+        }
+    }
+    return false;
 }
 
 std::string BitString::toString() const {
@@ -120,4 +169,8 @@ std::string BitString::toString() const {
         result += (bitArray[i] == 1) ? '1' : '0';
     }
     return result;
+}
+
+size_t BitString::getSize() const {
+    return arraySize;
 }
